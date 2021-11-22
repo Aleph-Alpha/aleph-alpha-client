049c1dcab10ccb2205b5490d95a74da012ecd247
Markus Klein <markus-klein@live.de>
2021-11-22 09:31:15 +0100

    use scm version

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name="aleph_alpha_client",
     url="https://github.com/Aleph-Alpha/aleph-alpha-client",
     author="Aleph Alpha",
-    author_email="info@aleph-alpha.de",
+    author_email="support@aleph-alpha.de",
     packages=["aleph_alpha_client"],
+    setup_requires=['setuptools_scm'],
     install_requires=["requests"],
     tests_require=["pytest", "pytest-cov", "python-dotenv"],
     extras_require={
         "test": ["pytest", "pytest-cov", "python-dotenv"],
     },
-    version="1.0.0",
+    use_scm_version=True,
     license="MIT",
     description="python client to interact with Aleph Alpha api endpoints",
-    long_description=open("README.md").read(),
+    long_description=readme(),
     long_description_content_type="text/markdown",
 )
