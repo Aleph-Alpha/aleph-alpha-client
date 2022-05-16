ccb5945d2bbc3f6b24926240f2d8fa7213594596
Julius Kreuzer <julius.kreuzer@aleph-alpha.com>
2022-05-16 12:42:40 +0200

    Use `__version__` attribute as the packaging version

--- setup.py
+++ setup.py
@@ -4,21 +4,26 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+
+def version():
+    exec(open('aleph_alpha_client/version.py').read())
+    return locals()["__version__"]
+
+
 setup(
     name="aleph-alpha-client",
     url="https://github.com/Aleph-Alpha/aleph-alpha-client",
     author="Aleph Alpha",
     author_email="support@aleph-alpha.com",
     packages=["aleph_alpha_client"],
-    setup_requires=['setuptools_scm'],
     install_requires=["requests"],
     tests_require=["pytest", "pytest-cov", "python-dotenv"],
     extras_require={
         "test": ["pytest", "pytest-cov", "python-dotenv"],
     },
-    use_scm_version=True,
     license="MIT",
     description="python client to interact with Aleph Alpha api endpoints",
     long_description=readme(),
     long_description_content_type="text/markdown",
+    version=version(),
 )
