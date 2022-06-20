0152b4c78d3eceb019b0bc0d3b2a3472afe8daef
mayukhdeb <mayukhmainak2000@gmail.com>
2022-06-20 19:16:22 +0530

    add support for str prompt

--- aleph_alpha_client/explanation.py
+++ aleph_alpha_client/explanation.py
@@ -4,19 +4,23 @@ from aleph_alpha_client.prompt import _to_prompt_item
 
 
 class ExplanationRequest(NamedTuple):
-    prompt: List[Union[str, ImagePrompt]]
+    prompt: Union[str, List[Union[str, ImagePrompt]]]
     target: str
     directional: bool
     suppression_factor: float
     conceptual_suppression_threshold: Optional[float] = None
 
-
     def render_as_body(self, model: str, hosting=Optional[str]) -> dict:
+
+        if isinstance(self.prompt, list):
+            prompt = [_to_prompt_item(item) for item in self.prompt]
+        else:
+            prompt = _to_prompt_item(prompt)
         return {
             "model": model,
-            "prompt": [_to_prompt_item(item) for item in self.prompt],
+            "prompt": prompt,
             "target": self.target,
             "suppression_factor": self.suppression_factor,
             "directional": self.directional,
-            "conceptual_suppression_threshold": self.conceptual_suppression_threshold
+            "conceptual_suppression_threshold": self.conceptual_suppression_threshold,
         }
