c036ff1e42aea912f7d39671fdb32630cade5f9c
Andreas Hartel <andreas.hartel@aleph-alpha.com>
2022-11-11 15:48:31 +0100

    fix tests

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -25,20 +25,6 @@ def test_translate_errors():
         _raise_for_status(response.status_code, response.text)
 
 
-def test_timeout(httpserver: HTTPServer):
-    def handler(foo):
-        time.sleep(2)
-
-    path = "/version"
-    httpserver.expect_request(path).respond_with_handler(handler)
-
-    """Ensures Timeouts works. AlephAlphaClient constructor calls version endpoint."""
-    with pytest.raises(requests.exceptions.ConnectionError):
-        AlephAlphaClient(
-            host=httpserver.url_for(""), token="AA_TOKEN", request_timeout_seconds=0.1
-        )
-
-
 def expect_call_respond_error(
     httpserver: HTTPServer, path: str, num_calls_expected: int, error_code: int
 ):
@@ -46,19 +32,6 @@ def expect_call_respond_error(
         httpserver.expect_ordered_request(path).respond_with_data(
             f"error({i})", status=error_code
         )
-
-
-def test_retry_sync(httpserver: HTTPServer):
-    path = "/version"
-    expect_call_respond_error(
-        httpserver,
-        path,
-        num_calls_expected=2,
-        error_code=HTTPStatus.SERVICE_UNAVAILABLE,
-    )
-    httpserver.expect_ordered_request(path).respond_with_data("ok", status=200)
-
-    AlephAlphaClient(host=httpserver.url_for(""), token="AA_TOKEN")
 
 
 def test_retry_sync_post(httpserver: HTTPServer):
@@ -82,6 +55,19 @@ def test_retry_sync_post(httpserver: HTTPServer):
     client = AlephAlphaClient(host=httpserver.url_for(""), token="AA_TOKEN")
     model = AlephAlphaModel(client, "MODEL")
     model.complete(request=request)
+
+
+def test_retry_sync(httpserver: HTTPServer):
+    path = "/version"
+    expect_call_respond_error(
+        httpserver,
+        path,
+        num_calls_expected=2,
+        error_code=HTTPStatus.SERVICE_UNAVAILABLE,
+    )
+    httpserver.expect_ordered_request(path).respond_with_data("ok", status=200)
+
+    AlephAlphaClient(host=httpserver.url_for(""), token="AA_TOKEN")
 
 
 async def test_retry_async(httpserver: HTTPServer):
@@ -117,3 +103,20 @@ async def test_retry_async_post(httpserver: HTTPServer):
 
     async with AsyncClient(token="AA_TOKEN", host=httpserver.url_for("")) as client:
         await client.complete(request, model="FOO")
+
+
+# This test should stay last in this file because it usese a sleeping handler.
+# This somehow blocks the plugin from clearing the httpserver state and thus
+# causes crosstalk with other tests.
+def test_timeout(httpserver: HTTPServer):
+    def handler(foo):
+        time.sleep(1)
+
+    path = "/version"
+    httpserver.expect_request(path).respond_with_handler(handler)
+
+    """Ensures Timeouts works. AlephAlphaClient constructor calls version endpoint."""
+    with pytest.raises(requests.exceptions.ConnectionError):
+        AlephAlphaClient(
+            host=httpserver.url_for(""), token="AA_TOKEN", request_timeout_seconds=0.1
+        )
