8604de9bb900242cc228c9d2509faa44c8dd2596
Julius Kreuzer <julius.kreuzer@aleph-alpha.com>
2022-12-01 10:52:56 +0100

    Set correct version of package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,12 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import aleph_alpha_client
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'aleph-alpha-client'
 copyright = '2022, Aleph Alpha'
 author = 'Aleph Alpha'
+version = aleph_alpha_client.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
