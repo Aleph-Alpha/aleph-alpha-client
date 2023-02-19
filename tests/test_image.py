cbf0c2f8febc0c901a36467979bbbaf976c277d0
PiotrMazurek <piotr.mazurek@aleph-alpha.com>
2023-02-19 16:14:07 +0100

    Fix issues with tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -5,7 +5,7 @@ from pytest import raises
 from pytest_httpserver import HTTPServer
 from requests import RequestException
 
-from aleph_alpha_client.image import Image, ImagePrompt
+from aleph_alpha_client.image import Image
 
 
 def test_from_url_with_non_OK_response(httpserver: HTTPServer):
@@ -20,16 +20,18 @@ def test_from_url_with_non_OK_response(httpserver: HTTPServer):
 
 def test_from_image_source_on_local_file_path_pathlib():
     image_source_path = Path(__file__).parent / "dog-and-cat-cover.jpg"
-    img_1 = ImagePrompt.from_image_source(image_source=image_source_path)
-    img_2 = ImagePrompt.from_file(path=image_source_path)
+    str_image_source_path = str(image_source_path)
+
+    img_1 = Image.from_image_source(image_source=image_source_path)
+    img_2 = Image.from_file(path=str_image_source_path)
     assert img_1.base_64 == img_2.base_64
 
 
 def test_from_image_source_on_local_file_path_str():
     image_source_path = Path(__file__).parent / "dog-and-cat-cover.jpg"
     image_source_path = str(image_source_path)
-    img_1 = ImagePrompt.from_image_source(image_source=image_source_path)
-    img_2 = ImagePrompt.from_file(path=image_source_path)
+    img_1 = Image.from_image_source(image_source=image_source_path)
+    img_2 = Image.from_file(path=image_source_path)
     assert img_1.base_64 == img_2.base_64
 
 
@@ -37,20 +39,18 @@ def test_from_image_source_on_url():
     image_source_url = (
         "https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png"
     )
-    img_1 = ImagePrompt.from_image_source(image_source=image_source_url)
-    img_2 = ImagePrompt.from_url(url=image_source_url)
+    img_1 = Image.from_image_source(image_source=image_source_url)
+    img_2 = Image.from_url(url=image_source_url)
     assert img_1.base_64 == img_2.base_64
 
 
 def test_from_image_source_on_bytes():
-    with open(Path(__file__).parent / "dog-and-cat-cover.jpg", "rb") as f:
-        image_source_bytes = f.read()
-
-    img_1 = ImagePrompt.from_image_source(image_source=image_source_bytes)
-    img_2 = ImagePrompt.from_bytes(bytes=image_source_bytes)
+    image_source_bytes = (Path(__file__).parent / "dog-and-cat-cover.jpg").read_bytes()
+    img_1 = Image.from_image_source(image_source=image_source_bytes)
+    img_2 = Image.from_bytes(bytes=image_source_bytes)
     assert img_1.base_64 == img_2.base_64
 
 
-def test_from_image_source_on_bytes():
+def test_from_image_source_on_input_of_a_wrong_type():
     with raises(TypeError):
-        ImagePrompt.from_image_source(image_source=None)
+        Image.from_image_source(image_source=None)
