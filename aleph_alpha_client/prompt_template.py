108b76073ae730d69d897fdb76c4eb459c8d5b0c
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-08-22 09:19:53 +0200

    Add pydoc to PromptTemplate

--- aleph_alpha_client/prompt_template.py
+++ aleph_alpha_client/prompt_template.py
@@ -4,8 +4,30 @@ from aleph_alpha_client.prompt import Prompt
 
 
 class PromptTemplate:
+    """Allows to build a `Prompt` using the `liquid template language <https://shopify.github.io/liquid/>`_.
+
+    Example:
+        >>> template = PromptTemplate(
+            '''{%- for name in names -%}
+            Hello {{name}}!
+            {% endfor -%}
+            ''')
+        >>> names = ["World", "Rutger"]
+        >>> prompt = template.to_prompt(names=names)
+        >>> request = CompletionRequest(prompt=prompt)
+    """
+
     def __init__(self, template_str: str) -> None:
+        """Initialize with the liquid template string.
+
+        Parameters:
+            template_str: the liquid template string
+        """
         self.template = Template(template_str)
 
     def to_prompt(self, **kwargs) -> Prompt:
+        """Creates a `Prompt` from the template string and the given parameters.
+
+        Provided parameters are passed to `liquid.Template.render`.
+        """
         return Prompt.from_text(self.template.render(**kwargs))
